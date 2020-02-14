014983c668c0313993f7a4beae55e4b8cba876b5
Marion <marion_ott@me.com>
2020-02-14 20:52:24 +0100

    Updates

---
+++ precache-manifest.8f65956d5562df4504db52262b06c332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d05307c49dfa4ac4a169721c49e99e",
+    "url": "/hopdeco-replicate/index.html"
+  },
+  {
+    "revision": "7fa6a18094e8740e9a8d",
+    "url": "/hopdeco-replicate/static/css/main.eaa243b4.chunk.css"
+  },
+  {
+    "revision": "c3470a1840f2d57eb712",
+    "url": "/hopdeco-replicate/static/js/2.9f4af176.chunk.js"
+  },
+  {
+    "revision": "7fa6a18094e8740e9a8d",
+    "url": "/hopdeco-replicate/static/js/main.d873a2f5.chunk.js"
+  },
+  {
+    "revision": "79c6111140e64b167132",
+    "url": "/hopdeco-replicate/static/js/runtime-main.f6283b49.js"
+  }
+]);
