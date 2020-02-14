8eabdfdb8a43ca577df900ee1f6676e78e70af79
Marion <marion_ott@me.com>
2020-02-14 20:56:58 +0100

    add github page live version && css quick fix

---
+++ build/precache-manifest.c7a06eb34506c2bd4691f176eb464f3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066e7b3051843d6f532e507c5ae8bed8",
+    "url": "/hopdeco-replicate/index.html"
+  },
+  {
+    "revision": "b410f96b8c424b393123",
+    "url": "/hopdeco-replicate/static/css/main.adc4a630.chunk.css"
+  },
+  {
+    "revision": "c3470a1840f2d57eb712",
+    "url": "/hopdeco-replicate/static/js/2.9f4af176.chunk.js"
+  },
+  {
+    "revision": "b410f96b8c424b393123",
+    "url": "/hopdeco-replicate/static/js/main.d873a2f5.chunk.js"
+  },
+  {
+    "revision": "79c6111140e64b167132",
+    "url": "/hopdeco-replicate/static/js/runtime-main.f6283b49.js"
+  }
+]);
